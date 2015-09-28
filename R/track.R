582f199b23abe0ff225ee4964742905ca6b9cb22
John M. Chambers <jmc@stat.stanford.edu>
2015-09-28 14:01:36 -0700

    add track class

---
+++ R/track.R
@@ -0,0 +1,26 @@
+degree <- setClass("degree",
+                   contains = "numeric")
+
+setValidity("degree",
+   function(object) {
+       nbad <-
+           length(object) -
+              sum(is.na(object) |
+                 (object >= -180. & object <= 180.))
+      if(nbad)
+         gettextf("%d values out of range +/- 180.", as.integer(nbad))
+      else
+         TRUE
+   } )
+
+
+track <- setClass("track",
+    slots = list(lat = "degree", long = "degree",
+                 time = "DateTime"))
+
+setMethod("initialize", "track",
+    function(.Object, lat, long, ...) {
+        .Object@lat <- degree(lat)
+        .Object@long <- degree(long)
+        callNextMethod(.Object, ...)
+    })
